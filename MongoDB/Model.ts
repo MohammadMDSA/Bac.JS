c62bddfc47457e3dba88a560f313f93574173401
Mohammad MDSA <samimd.77@gmail.com>
2018-05-27 18:30:16 +0430

    Added  model and schema handle

--- MongoDB/Model.ts
+++ MongoDB/Model.ts
@@ -1,7 +1,12 @@
-import {Schema, model} from "mongoose";
+import {Schema, model, SchemaDefinition, Model, Document} from "mongoose";
 
-export default abstract class Model {
+export default abstract class DBModel {
 	
+	private static $schema(): SchemaDefinition { return null; }
 
+	public static get $model(): Model<Document> {
+		const schema: Schema = new Schema(this.$schema());
+		return model(this.constructor.name, schema);
+	}
 
 }
