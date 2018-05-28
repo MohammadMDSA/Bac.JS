4be862a73d3f6cb87e76a5b24f8caa61e44cd427
Mohammad MDSA <samimd.77@gmail.com>
2018-05-28 13:56:09 +0430

    Added mongo options

--- server/config.ts
+++ server/config.ts
@@ -1,10 +1,19 @@
+import { ConnectionOptions } from "mongoose";
+
 export default interface IConfig {
     routers: IRouter[];
 
     port?: number;
+
+    mongo?: IMongoConfig;
 }
 
 export interface IRouter {
     prefix: string;
     route: IRouter[] | string;
+}
+
+interface IMongoConfig {
+    connection: string;
+    options?: ConnectionOptions;
 }
