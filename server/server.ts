5cf57ee60a45a523e46d3ca8a87fced2c53fb5b5
Mohammad MDSA <samimd.77@gmail.com>
2018-05-28 13:57:00 +0430

    Add mongo db handler

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import { IHandler, RequestType } from "./controller";
 import IConfig from "./config";
 import { promises } from "fs";
 import Colors =  require("colors");
+import { connect } from "mongoose";
 
 export default class Server {
 
@@ -23,8 +24,10 @@ export default class Server {
     public async start(): Promise<void> {
 		await this.handleRouters("", this._config.routers);
 
+		await this.connectMongo();
+
 		await this.server.start();
-		console.log(`started on ${this.server.settings.port}`);
+		console.log(Colors.green(`started on ${this.server.settings.port}`));
 	}
 
 	private async handleRouters(prefix: string, router: IRouter[]): Promise<void> {
@@ -62,6 +65,18 @@ export default class Server {
 			console.log();
 		} catch (error) {
 			console.log("Could not assign route with prefix: ".red + prefix.green + " from ".red + ("." + path).green);
+		}
+	}
+
+	private async connectMongo(): Promise<void> {
+		try {
+			if (!this._config.mongo) {
+				return;
+			}
+	
+			await connect(this._config.mongo.connection, this._config.mongo.options);
+		} catch (e) {
+			console.log(Colors.red(`Failed to connect to Mongo server of ${this._config.mongo.connection}`))
 		}
 	}
 
