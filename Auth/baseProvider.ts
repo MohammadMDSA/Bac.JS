e9c2b43cc965942778fa5ebb7af58c4a0c0a77cc
Mohammad MDSA <samimd.77@gmail.com>
2018-05-29 17:33:44 +0430

    Added base provider for auth

---
+++ Auth/baseProvider.ts
@@ -0,0 +1,28 @@
+import UserModel from "./user";
+import { Document, Model } from "mongoose";
+
+export default abstract class BaseProvider {
+    protected _options: IOptions;
+
+    constructor(options: IOptions) {
+        this._options = options;
+        if (!this._options.userModel) {
+            this._options.userModel = require("./user");
+        }
+    }
+
+    public abstract authtoken(token: string);
+
+    public abstract getToken(user, request, client): string;
+
+    public abstract findByUsername(username: string);
+
+    public abstract validateUser(user);
+
+    public abstract validatePassword(user, password);
+}
+
+export interface IOptions {
+    userModel?: Model<Document>;
+    secret: string;
+}
