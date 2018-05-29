ff9b143fe319df2934857b508ad3e0a27ecea7f8
Mohammad MDSA <samimd.77@gmail.com>
2018-05-29 11:38:30 +0430

    Test: Added any method

--- controllers/test.ts
+++ controllers/test.ts
@@ -1,17 +1,17 @@
-import Controller from "../server/controller";
+import Controller, { AnyRequestType } from "../server/controller";
 import { RequestType } from "../server/controller";
 import TestModel from "../models";
 
 export default class Test extends Controller {
 
     public init(): void {
-        this.assign([RequestType.GET, RequestType.POST], async (req, h) => {
-            let e = new TestModel({Name: "hey", foo: 13, Username: false});
-            await e.save();
-            return e;
-        });
+        // this.assign([RequestType.GET, RequestType.POST], async (req, h) => {
+        //     let e = new TestModel({Name: "hey", foo: 13, Username: false});
+        //     await e.save();
+        //     return e;
+        // });
 
-        this.assign([RequestType.DELETE], (request, h) => {
+        this.assign(AnyRequestType.Any, (request, h) => {
             return {o: "SomeObject"};
         });
     }
