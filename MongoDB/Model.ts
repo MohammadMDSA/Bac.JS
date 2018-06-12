c3943517ad084af66bcf287ae27ddd04195555b6
Mohammad MDSA <samimd.77@gmail.com>
2018-06-12 21:53:16 +0430

    Added generic doc type

--- MongoDB/Model.ts
+++ MongoDB/Model.ts
@@ -4,7 +4,7 @@ export default abstract class DBModel {
 
 	protected static $schema(): SchemaDefinition { return null; }
 
-	public static $model(): Model<Document> {
+	public static $model<T extends Document>(): Model<T> {
 		const schema: Schema = new Schema(this.$schema());
 
 		return model(this.prototype.constructor.name, schema);
