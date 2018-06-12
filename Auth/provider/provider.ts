0fb556da4dc0c6078fc96d1070be2646fde1d9e4
Mohammad MDSA <samimd.77@gmail.com>
2018-06-12 21:53:01 +0430

    Added simiple signup

--- Auth/provider/provider.ts
+++ Auth/provider/provider.ts
@@ -1,6 +1,9 @@
 import ProviderBase, { IProviderOptions } from "./providerBase";
 import { Request, ResponseToolkit } from "hapi";
 import { ValidationResult } from "hapi-auth-jwt2";
+import { jwtDecode } from "../utils";
+import { DecodeOptions } from "jsonwebtoken";
+import User, { IUserModelDocument } from "../user";
 
 export default class Provider extends ProviderBase<IDefaultProviderOptions> {
 
@@ -8,7 +11,10 @@ export default class Provider extends ProviderBase<IDefaultProviderOptions> {
 		super(options);
 	}
 
-	public authenticateToken(token: string): ValidationResult {
+	public authenticateToken(token: string, options?: DecodeOptions): ValidationResult {
+
+		let decoded = jwtDecode(token, options);
+
 		return null;
 	}
 
@@ -29,6 +35,33 @@ export default class Provider extends ProviderBase<IDefaultProviderOptions> {
 				credentials: null
 			};
 		}
+	}
+
+	public SignUp(username: string, password: string, email: string): IUserModelDocument {
+
+		if (!username) {
+			throw new Error("Invalid username!");
+		}
+
+		if (!password) {
+			throw new Error("Invalid password!");
+		}
+
+		if (!email) {
+			throw new Error("Invalid email address!");
+		}
+
+		let user = new User({
+			username: username,
+			password: password,
+			email: email
+		});
+
+		user.save;
+
+		console.log(user);
+
+		return user;
 	}
 }
 
