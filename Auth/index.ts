ad0f8d2ab52632bdde370059aff8e16bbf3ce61d
Mohammad MDSA <samimd.77@gmail.com>
2018-06-12 21:52:12 +0430

    Registered auth controller

--- Auth/index.ts
+++ Auth/index.ts
@@ -1,27 +1,34 @@
 import * as JWT from "hapi-auth-jwt2";
-import { Server, ServerRegisterPluginObject } from "hapi";
 import Provider from "./provider/provider";
 import Plugin, { IPluginOptions } from "../server/pluginBase";
 import * as Hapi from "hapi";
+import Auth from "./AuthController";
+import Server from "../server/server";
 
 export default class AuthPlugin extends Plugin<IAuthOptions> {
+	
+	private _provider: Provider;
+
 	constructor(server: Server, options: IAuthOptions) {
 		super(server, options);
+		this._provider = new Provider({secret: this._options.secret});
 	}
 
 	public async register(): Promise<void> {
-		await this._server.register(JWT);
+		await this._server.CoreServer.register(JWT);
 
-		let provider = new Provider({secret: this._options.secret});
 
 		let authOptions: JWT.Options = {
 			key: this._options.secret,
-			validate: provider.verify,
+			validate: this._provider.verify,
 			verifyOptions: { algorithms: ["HS256"] }
 		};
 
-		this._server.auth.strategy("jwt", "jwt", authOptions);
-		this._server.auth.default("jwt");
+		this._server.CoreServer.auth.strategy("jwt", "jwt", authOptions);
+		this._server.CoreServer.auth.default("jwt");
+
+		let authController = new Auth(this._provider);
+		this._server.assignHandlers(authController.handlers, authController.prefix);
 	}
 }
 
