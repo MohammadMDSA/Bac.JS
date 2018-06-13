da267459b92a01e0bd1f14d06eabe869027f431c
Mohammad MDSA <samimd.77@gmail.com>
2018-06-13 15:29:08 +0430

    Added signing type

--- Auth/utils.ts
+++ Auth/utils.ts
@@ -1,6 +1,8 @@
 import * as JWT from "jsonwebtoken";
+import { Request } from "hapi";
+import { ITokenObject } from "./provider/provider";
 
-export function jwtSign(message, key: JWT.Secret, options?: JWT.SignOptions): string {
+export function jwtSign(message: ITokenObject, key: JWT.Secret, options?: JWT.SignOptions): string {
     return JWT.sign(message, key, options);
 }
 
