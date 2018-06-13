78c33143e298db759724c19f5c04a5fcf7755f28
Mohammad MDSA <samimd.77@gmail.com>
2018-06-14 04:28:49 +0430

    Added session interface

--- Auth/user.ts
+++ Auth/user.ts
@@ -10,7 +10,7 @@ class User extends DBModel {
             password: { type: String },
             sessions: [
                 {
-                    createdAt: { type: Date, default: Date.now },
+                    createdAt: { type: Date, default: Date.now() },
                     ip: {type: String}
                 }
             ]
@@ -25,10 +25,10 @@ export interface IUserModelDocument extends Document {
     username: string;
     password: string;
     email: string;
-    session: [
-        {
-            createdAt: Date,
-            ip: string
-        }
-    ];
+    sessions: ISession[];
+}
+
+export interface ISession {
+    createdAt: number;
+    ip: string;
 }
