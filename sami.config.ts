8e9b949238504068a7d3f5b13b3f5b41716b29aa
Mohammad MDSA <samimd.77@gmail.com>
2018-06-15 19:17:58 +0430

    Test: Testing longend expiration

--- sami.config.ts
+++ sami.config.ts
@@ -26,7 +26,7 @@ export default appConfig = {
         session: {
             limited: true,
             limitation: 4,
-            expiredAfter: Date.UTC(0, 0, 0, 0, 1, 0) - Date.UTC(0, 0, 0, 0, 0, 0)
+            expiredAfter: Date.UTC(0, 1, 0, 0, 0, 0) - Date.UTC(0, 0, 0, 0, 0, 0)
         }
     }
 };
