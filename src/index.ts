b0fa1f0d26a761bb9e199ce4bda7faa6c4f22b56
Mohammad MDSA <samimd.77@gmail.com>
2018-07-22 23:23:54 +0430

    Added package index

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Server from "./server/server";
+import IConfig, {IRouter, IAuth, IMongoConfig} from "./server/config";
+import Controller, {AnyRequestType, IControllerOptions, IHandler, RequestType} from "./server/controller";
+import DBModel from "./MongoDB/Model";
+
+export {
+    // Server file
+    Server,
+
+    // Config file
+    IRouter,
+    IAuth,
+    IMongoConfig,
+    IConfig,
+
+    // Controller file
+    Controller,
+    AnyRequestType,
+    IControllerOptions,
+    IHandler,
+    RequestType,
+
+    // Model
+    DBModel,
+
+};
--- src/server/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Server from "./server";
-import * as Controller from "./controller";
-import * as Config from "./config";
-
-export default {
-    Server,
-    Config,
-    Controller
-};
